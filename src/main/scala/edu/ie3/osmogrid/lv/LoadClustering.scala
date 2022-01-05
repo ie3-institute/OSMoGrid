1bcd7c54acc740efdae91464f68a6d779d674bc9
Kittl, Chris <chris.kittl@tu-dortmund.de>
2022-01-05 09:06:29 +0100

    Document a bit more
    what has to be done next.

--- src/main/scala/edu/ie3/osmogrid/lv/LoadClustering.scala
+++ src/main/scala/edu/ie3/osmogrid/lv/LoadClustering.scala
@@ -35,6 +35,18 @@ trait LoadClustering {
       Seq(loadLocations)
     }
 
-    // TODO: Build convex hulls of each sub group
+    /** TODO
+     *   1) Go through all sub groups and check, if they are fully connected. If not: Split them up.
+     *   2) Check for minimum cluster size and ignore those, that don't fulfill this requirement
+     *   For each sub group:
+     *   3) Calculate the number of needed clusters
+     *   4) Determine clusters
+     *   5) Rebuild a graph per cluster
+     *   6) Possibly assign a unique number to the cluster
+     *
+     * Needed data
+     * - Targeted power of a substation (maybe as a list?)
+     * - Minimum power of a substation
+     */
   }
 }
