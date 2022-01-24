f8900a8126ec11d484781f4e91cd6378da8f4eda
Kittl, Chris <chris.kittl@tu-dortmund.de>
2022-01-24 10:35:23 +0100

    Check logging results for dead runs

--- src/test/scala/edu/ie3/osmogrid/guardian/OsmoGridGuardianSpec.scala
+++ src/test/scala/edu/ie3/osmogrid/guardian/OsmoGridGuardianSpec.scala
@@ -6,27 +6,31 @@
 
 package edu.ie3.osmogrid.guardian
 
+import akka.actor.testkit.typed.CapturedLogEvent
 import akka.actor.testkit.typed.Effect.{MessageAdapter, Spawned}
 import akka.actor.testkit.typed.scaladsl.BehaviorTestKit
 import akka.actor.typed.{ActorRef, Behavior}
 import edu.ie3.osmogrid.cfg.OsmoGridConfigFactory
+import edu.ie3.osmogrid.guardian.OsmoGridGuardian.RunGuardianDied
 import edu.ie3.osmogrid.guardian.run.RunGuardian
 import edu.ie3.osmogrid.guardian.run.Request
 import edu.ie3.osmogrid.io.output.ResultListener.ResultEvent
 import edu.ie3.test.common.UnitSpec
+import org.slf4j.event.Level
 
 import java.util.UUID
 
 class OsmoGridGuardianSpec extends UnitSpec {
   "Having an overall OsmoGridGuardian" when {
     "being idle" should {
-      "spawn a new RunGuardian on request" in {
-        val guardianData = OsmoGridGuardian.GuardianData(Seq.empty[UUID])
-        val config = OsmoGridConfigFactory.defaultTestConfig
-        val additionalListeners = Seq.empty[ActorRef[ResultEvent]]
-        val runId = UUID.randomUUID()
+      val guardianData = OsmoGridGuardian.GuardianData(Seq.empty[UUID])
+      val config = OsmoGridConfigFactory.defaultTestConfig
+      val additionalListeners = Seq.empty[ActorRef[ResultEvent]]
+      val runId = UUID.randomUUID()
 
-        val idleTestKit = BehaviorTestKit(OsmoGridGuardian.idle(guardianData))
+      val idleTestKit = BehaviorTestKit(OsmoGridGuardian.idle(guardianData))
+
+      "spawn a new RunGuardian on request" in {
         idleTestKit.run(
           OsmoGridGuardian.Run(config, additionalListeners, runId)
         )
@@ -38,6 +42,15 @@ class OsmoGridGuardianSpec extends UnitSpec {
           case Spawned(childBehav, _, _) =>
             fail(s"Spawned a child with wrong behavior '$childBehav'.")
         }
+      }
+
+      "report a dead RunGuardian" in {
+        idleTestKit.run(RunGuardianDied(runId))
+
+        idleTestKit.logEntries() should contain only CapturedLogEvent(
+          Level.INFO,
+          s"Run $runId terminated."
+        )
       }
     }
   }
