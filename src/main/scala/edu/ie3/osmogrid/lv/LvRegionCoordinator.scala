91dc6cac6a03bc8a1bd67f6f5af030ed90925a34
Sebastian Peter <14994800+sebastian-peter@users.noreply.github.com>
2022-01-29 21:08:12 +0100

    Cherrypicking changes from ck/#90

--- src/main/scala/edu/ie3/osmogrid/lv/LvRegionCoordinator.scala
+++ src/main/scala/edu/ie3/osmogrid/lv/LvRegionCoordinator.scala
@@ -6,14 +6,24 @@
 
 package edu.ie3.osmogrid.lv
 
-import akka.actor.typed.scaladsl.Behaviors
-
 import akka.actor.typed.ActorRef
+import akka.actor.typed.scaladsl.Behaviors
 
 object LvRegionCoordinator {
   sealed trait Request
+  final case class Partition(replyTo: ActorRef[Response])
+      extends Request // TODO: OSM data needs to be transferred
+
+  sealed trait Response
+  object Done extends Response
 
   def apply(
       lvGeneratorPool: ActorRef[LvGenerator.Request]
-  ): Behaviors.Receive[Request] = ???
+  ): Behaviors.Receive[Request] = idle
+
+  private def idle: Behaviors.Receive[Request] = Behaviors.receive {
+    case (ctx, unsupported) =>
+      ctx.log.warn(s"Received unsupported message '$unsupported'.")
+      Behaviors.stopped
+  }
 }
